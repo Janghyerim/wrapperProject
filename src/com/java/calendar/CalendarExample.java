b3f94d3e315ca44c2fc14e39b79dc2508019f6cc
Janghyerim <wns8385@naver.com>
2022-10-17 12:16:31 +0900

    +

--- src/com/java/calendar/CalendarExample.java
+++ src/com/java/calendar/CalendarExample.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class CalendarExample { //달력 만들기
 
-	public static void main(String[] args) {///////
+	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
